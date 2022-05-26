d5d9421831d38f7a1fa9ccd02361431419b429a1
viktormuzyka <vmusika007@gmail.com>
2022-05-26 15:46:58 +0300

    create functions for polynomial ring

--- main_test.cpp
+++ main_test.cpp
@@ -7,81 +7,97 @@
 #include "MillerRabinTest.h"
 #include "montgomery_form.h"
 #include "inverse.h"
-
 using namespace std;
+polynomial polRingAddition(const string& a, const string& b){
+	polynomial test_1(a);
+	polynomial test_2(b);
+	return test_1+test_2;
+}
+polynomial polRingSubtraction(const string& a, const string& b){
+	polynomial test_1(a);
+	polynomial test_2(b);
+	return test_1-test_2;
+}
+polynomial polRingMultiply(const string& a, const string& b){
+	polynomial test_1(a);
+	polynomial test_2(b);
+	return test_1*test_2;
+}
+polynomial polRingDerivative(const string& a){
+	polynomial test(a);
+	return test.derivate();
+}
+bigint polRingValueInPoint(const string& a, const string& b){
+	polynomial test(a);
+	bigint value(b);
+	return test.get_value(value);
+}
+
+bigint finFieldAddition(const string& a, const string& b){
+	bigint first(a);
+	bigint second(b);
+	return first+second;
+}
+bigint finFieldSubstraction(const string& a, const string& b){
+	bigint first(a);
+	bigint second(b);
+	return first-second;
+}
+bigint finFieldMultiplication(const string& a, const string& b){
+	bigint first(a);
+	bigint second(b);
+	return first*second;
+}
+bigint finFieldInverse(const string& first, const string& second){
+	bigint a(first);
+	bigint b(second);
+    bigint i = modInverse(a, b);
+    if (i==-1){
+    	return bigint(0);
+	} else {
+		return i;
+	}
+}
+bigint finFieldDivision(const string& a, const string& b){
+	bigint first(a);
+	bigint second(b);
+	return first/second;
+}
+/*bigint finFieldPow(const string& a, const string& b){
+	bigint first(a);
+	bigint second(b);
+	return first/second;
+}*/
+/*bigint finFieldFacrorizationNaive(const string& a){
+	bigint first(a);
+	std::vector<bigint> res = Factorization(first, false);
+	PrintFactors(test_data, res);
+	return first/second;
+}*/
+/*bigint finFieldFacrorizationPolard(const string& a){
+	bigint first(a);
+	std::vector<bigint> res = Factorization(first, true);
+	PrintFactors(test_data, res);
+	return first/second;
+}*/
 
 int main() {
-	//safroniuk coba
-	cout <<"	POLYNOMIAL" << endl;
-	cout <<"PART1: " << endl;
-	polynomial test_1("100x^2+x^3+12");
-	cout << test_1 << endl;
-	polynomial test_2("-100x^2-9000x^3+12x^1+123");
-	cout << test_2 << endl;
-	cout <<"Addition: " << test_1+test_2 << endl;
-	cout <<"Subtraction: " << test_1-test_2 << endl;
-	cout <<"Multiplication: " << test_1*test_2 <<endl;
-	cout << endl;
-	//bocharova muzyka
-	cout <<"PART2: " << endl;
-	polynomial p1("-10000x^-12-9000x^4200+12x^1+123");	
-	polynomial derivate_p1=p1.derivate();
-	cout << "Polynomial: "<< p1 << endl;
-	cout << "Value of polynmial at point 1: "<< p1.get_value(1) <<  endl;
-	cout << "Derivative: "<< derivate_p1 << endl;
-	cout << endl;
-	cout << endl;
-
-	cout <<"	FINITE FIELD" << endl;
-	//safroniuk coba
-	cout <<"Additional, Substraction, Multiplication: " << endl;
-	bigint a=1000000;
-	bigint b=2000000;
-	cout <<"Additional "<<a<<"+"<<b<<": " << a+b << endl;
-	cout <<"Multiplication "<<a<<"*"<<b<<": " << a*b << endl;
-	cout <<"Substraction "<<a<<"-"<<b<<": " << a-b << endl;
-	//doroshenko
-	a*=10;
-	cout << "Division "<<a<<"/"<<b<<": " << a/b << endl;
-	//bloshenko hrishenko
-	/*cout <<"Eylera and Carmichael function: " << endl;
-	bigint n = 90;
-    cout<<"Eylera("<<n<<") : "<<phi(n)<<endl;
-    cout<<"Carmichael("<<n<<") : "<<carmichael(n)<<endl;*/
-    //ilyin doroshenko
-    bigint x = 129_BI;
-    bigint y = 7_BI;
-    cout << "Inverse "<<x <<" mod "<<y << endl;
-    bigint i = modInverse(x, y);
-    if (i==-1){
-    	std::cout << std::endl;
-	} else {
-		std::cout << i << std::endl;
-	}
-    //kotlyarova prokopchuk
-    cout <<"Multiplication of numbers: " << endl;
-    bigint n = 100;
-	bool use_pollard = true;
-	std::vector<bigint> res = Factorization(n, use_pollard);
-	std::cout << "Naive method(100): ";
-	PrintFactors(n, res);
-	use_pollard = true;
-	res = Factorization(n, use_pollard);
-	std::cout << "Pollard method(100): ";
-	PrintFactors(n, res);
+	/*cout <<"	POLYNOMIAL" << endl;
+	cout << polRingAddition("100x^2+x^3+12", "-100x^2-9000x^3+12x^1+123")<< endl;
+	cout << polRingSubtraction("100x^2+x^3+12", "-100x^2-9000x^3+12x^1+123")<< endl;
+	cout << polRingMultiply("100x^2+x^3+12", "-100x^2-9000x^3+12x^1+123")<< endl;
+	cout << polRingDerivative("-10000x^-12-9000x^4200+12x^1+123") << endl;
+	cout <<  polRingValueInPoint("-10000x^-12-9000x^4200+12x^1+123", "1") <<  endl;
+	cout << endl;*/
 	
-	//khlopyk
-	/**/
-	
-	//bocharova hromadiuk
-	cout <<"Miller Rabin Test: " << endl;
-	BigInt Miiller_test = 100;
-	cout <<"Is prime 100(no==0): " << isPrime(Miiller_test,1) << endl;
-	Miiller_test = 2147483647;
-	cout <<"Is prime 2147483647(yes==1): " << isPrime(Miiller_test,1) << endl;
-	
-	//polishuk
-	cout <<"Rapid rise to the degree: " << endl;
+/*	cout <<"	FINITE FIELD" << endl;
+	cout <<finFieldAddition("10", "20")<< endl;
+	cout <<finFieldSubstraction("10", "20")<< endl;
+	cout <<finFieldMultiplication("10", "20")<< endl;
+	bigint inversAns = finFieldInverse("129", "7");
+	cout <<finFieldDivision("200", "20")<< endl;*/
+		//polishuk
+/*	cout <<"Rapid rise to the degree: " << endl;
     bigint bint1("10");
     bigint bint2("12");
     montgomery mont1(bint1, bigint("59"),bigint("64"));
@@ -92,11 +108,42 @@ int main() {
     mont3 = mont1 ^ mont2;
     bint2 = mont3.convertToStandartForm(mont3.getmontgform());
     cout << "Good!" << endl;
-    cout << endl;
+    cout << endl;*/
+    
+     	//kotlyarova prokopchuk
+    /*cout <<"Multiplication of numbers: " << endl;
+    bigint test_data = 100;
+	bool use_pollard = true;
+	std::vector<bigint> res = Factorization(test_data, use_pollard);
+	std::cout << "Naive method(100): ";
+	PrintFactors(test_data, res);
+	use_pollard = true;
+	res = Factorization(test_data, use_pollard);
+	std::cout << "Pollard method(100): ";
+	PrintFactors(test_data, res);*/
+
+	//bloshenko hrishenko
+	//CYCLED
+	/*cout <<"Eylera and Carmichael function: " << endl;
+	int n = 90;
+    cout<<"Eylera("<<n<<") : "<<phi(n)<<endl;
+    cout<<"Carmichael("<<n<<") : "<<carmichael(n)<<endl;*/
+
+	
+	//khlopyk
+	/**/
+	
+	//bocharova hromadiuk
+/*	cout <<"Miller Rabin Test: " << endl;
+	BigInt Miiller_test = 100;
+	cout <<"Is prime 100(no==0): " << isPrime(Miiller_test,1) << endl;
+	Miiller_test = 2147483647;
+	cout <<"Is prime 2147483647(yes==1): " << isPrime(Miiller_test,1) << endl;*/
+	
     
     
     //prokopchuk polishuk
-	cout <<"	POLYNOMIAL FIELD" << endl;
+/*	cout <<"	POLYNOMIAL FIELD" << endl;
 	cout <<"PART3: " << endl;
 	bigint number("-10");
 	polynomial_field field1("2x^4+3x^2-1x^1", bigint(7));
@@ -110,7 +157,7 @@ int main() {
 	c = field1 + field2;
 	cout <<"("<<field1<<")+("<<field2<<") = "<< c <<endl;
 	c = c * number;
-	cout <<"("<<field1<<")*("<<number<<") = "<< c <<endl;
+	cout <<"("<<field1<<")*("<<number<<") = "<< c <<endl;*/
 	
 	
 	
