5b7efcb5a0e6e34dede7cc6a44404051b8a586b6
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2022-05-26 10:58:55 +0300

    Added additional checks and fixed bug in modulo() function

--- polynomial_field.h
+++ polynomial_field.h
@@ -31,9 +31,9 @@ class polynomial_field {
 		add(pow, coefficient);
 	}
 
-		bool checkMod(bigint& mod_)
+	bool checkMod(bigint& mod_)
 	{
-		if (mod_ <= bigint("1")  )
+		if (mod_ <= bigint("1"))
 			return false;
 		if (mod_ <= bigint("3"))
 			return true;
@@ -47,23 +47,29 @@ class polynomial_field {
 			if (mod_ % i == 0 || mod_ % (i + 2) == 0)
 				return false;
 		}
-		
-		
+
+
 		return true;
 	}
 
-		bigint modulo(bigint bint)
+	bigint modulo(bigint bint) const
+	{
+		if (!bint.isPositive())
 		{
-			if (!bint.isPositive())
-			{
-				bigint temp = -(bint / modulus) + 1;
-				bint += temp * modulus;
-			}
-			else
-				bint = bint % modulus;
-
-			return bint;
+			bigint temp = -(bint / modulus) + 1;
+			bint += temp * modulus;
 		}
+		bint = bint % modulus;
+
+		return bint;
+	}
+
+	void checkFieldOrders(const polynomial_field& rhs) const {
+		if (this->modulus != rhs.modulus) {
+			throw std::invalid_argument("Fields have different orders");
+		}
+	}
+
 public:
 	polynomial_field() :modulus(1) {};
 
@@ -74,17 +80,18 @@ public:
 		{
 			if (!checkMod(mod_))
 				throw(std::invalid_argument("Invalid modulus"));
+
 			modulus = mod_;
 			for (auto i = 0; i < keys.size(); ++i) {
-				if (keys[i] != 0) data[i] = keys[i];
+				if (keys[i] != 0) data[i] = modulo(keys[i]);
 			}
 		}
-		catch (const std::exception&e)
+		catch (const std::exception& e)
 		{
-			std::cout << e.what()<<std::endl;
+			std::cout << e.what() << std::endl;
 		}
 
-		
+
 	}
 
 	polynomial_field(std::string str, bigint mod_ = 1) {
@@ -175,32 +182,33 @@ public:
 			}
 		}
 
-		
-		 
+
+
 		rhs.updateElem(pow, coefficient, signPow, signCoefficient, coefficientChanged);
 
 		return ss;
 	}
 
-	[[nodiscard]] polynomial_field derivate()  {
+	[[nodiscard]] polynomial_field derivative() const {
 		auto rhs = *this;
 		auto output = polynomial_field();
-		for (const auto& p : rhs.data) {
-			
-			output.data[p.first - 1]= modulo(output.data[p.first - 1] + p.second * (bigint)p.first);
 
+		for (const auto& p : rhs.data) {
+			output.data[p.first - 1] = modulo(output.data[p.first - 1] + p.second * (bigint)p.first);
 		}
+
+		output.trim();
 		return output;
 	}
 
-	[[nodiscard]] bigint get_value(bigint x)  {
+	[[nodiscard]] bigint get_value(bigint x) const {
 		auto rhs = *this;
 		bigint ans = 0;
-		auto output = polynomial_field();
+
 		for (const auto& p : rhs.data) {
 			ans += p.second * x.pow((bigint)p.first);
 		}
-		
+
 		return modulo(ans);
 	}
 
@@ -208,41 +216,56 @@ public:
 		return *this;
 	}
 
-	[[nodiscard]] polynomial_field operator-()  {
+	[[nodiscard]] polynomial_field operator-() const {
 		return (*this) * (-1);
 	}
 
-		[[nodiscard]] polynomial_field operator+(const polynomial_field& rhs) const {
+	[[nodiscard]] polynomial_field operator+(const polynomial_field& rhs) const {
+		checkFieldOrders(rhs);
+
 		auto output = *this;
+
 		for (const auto& p : rhs.data) {
-			output.data[p.first] = (output.data[p.first] + p.second ) %modulus;
+			output.data[p.first] = modulo(output.data[p.first] + p.second);
 		}
+
+		output.trim();
 		return output;
 	}
 
-	[[nodiscard]] polynomial_field operator-(const polynomial_field& rhs)  {
+	[[nodiscard]] polynomial_field operator-(const polynomial_field& rhs) const {
+		checkFieldOrders(rhs);
+
 		auto output = *this;
 		for (const auto& p : rhs.data) {
 			output.data[p.first] = modulo(output.data[p.first] - p.second);
 		}
+
+		output.trim();
 		return output;
 	}
 
-	[[nodiscard]] polynomial_field operator*(const polynomial_field& rhs)  {
+	[[nodiscard]] polynomial_field operator*(const polynomial_field& rhs) const {
+		checkFieldOrders(rhs);
+
 		auto output = polynomial_field();
 		for (const auto& p1 : (*this).data) {
 			for (const auto& p2 : rhs.data) {
 				output.data[p1.first + p2.first] = modulo(output.data[p1.first + p2.first] + p1.second * p2.second);
 			}
 		}
+
+		output.trim();
 		return output;
 	}
 
-	[[nodiscard]] polynomial_field operator*(const bigint& number)  {
+	[[nodiscard]] polynomial_field operator*(const bigint& number) const {
 		auto output = *this;
 		for (const auto& p : output.data) {
 			output.data[p.first] = modulo(output.data[p.first] * number);
 		}
+
+		output.trim();
 		return output;
 	}
 
