b44727ce2c111c019789b71805cbd8a76222e1ee
Vlad Bloshenko <71842735+blotenko@users.noreply.github.com>
2022-05-27 14:41:31 +0300

    функции кармайлка/еурела  работающие на основе разложения на множители

--- Euler_func_and karmayrl_func.cpp
+++ Euler_func_and karmayrl_func.cpp
@@ -1,79 +1,162 @@
-//
-//  main.cpp
-//  Euler_func_and karmayrl_func
-//
-//  Created by Blotenko on 06.05.2022.
-//
-
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include "bigint.h"
+#include "big_int.hpp"
 
 
 using namespace std;
 
-
-bigint phi (bigint n) {
-    bigint result = n;
-    for (bigint i=2; i*i<=n; i=i+1)
-        if ((n % i )== 0) {
-            while (n % i == 0)
-                n /= i;
-            result -= result / i;
-        }
-    if (n > 1)
-        result  -= result / n;
-    return result;
-    
-}
-
-
-
-bigint restOfPot(bigint a,int b,bigint T)
+bool isPrime(bigint n)
 {
-    a=a%T;
-    bigint R=1;
-    while(b)
-    {
-        if(b&1) R=(R*a)%T;
-        a=(a*a)%T;
-        b>>=1;
+    if (n <= 1)
+        return false;
+    if (n <= 3) {
+        return true;
     }
-    return (R);
+
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+
+    for (int i = 5; bigint(i * i) <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    return true;
+
 }
 
 
-bigint gcd(bigint a, bigint b)
-{
-    if (a==0)
+
+bigint gcd(bigint a, bigint b) {
+    if (a == 0)
         return b;
-    return gcdDDD(b % a, a);
+    if (b == 0)
+        return a;
+
+    if (a == b)
+        return a;
+
+    if (a > b)
+        return gcd(a - b, b);
+    return gcd(a, b - a);
 }
 
 
-int carmichael(bigint n) {
-    int k = 1;
-  for( ;; ) {
-    int done = 1;
-    for( bigint x=1; x<n; x++ ) {
-      if( gcd(x,n)==1 && restOfPot(x, k, n)!= 1 ) {
-        done = 0;
-        k++;
-      }
+
+bigint carmaicle(vector<bigint> arr, int n)
+{
+    bigint ans = arr[0];
+ 
+    for (int i = 1; i < n; i++)
+        ans = (((arr[i] * ans)) /
+                (gcd(arr[i], ans)));
+ 
+    return ans;
+}
+
+
+
+bigint eurel(vector<bigint> arr, int n)
+{
+    bigint ans = arr[0];
+ 
+    for (int i = 1; i < n; i++)
+        ans = ((arr[i] * ans));
+ 
+    return ans;
+}
+
+
+
+
+
+
+bigint PollardRho(bigint n)
+{
+    srand(std::time(NULL));
+
+    if (n == 1) return n;
+
+    if (n % 2 == 0) return 2;
+
+
+    bigint x = (bigint(rand()) % (n - 2)) + 2;
+    bigint y = x;
+
+    bigint c = (bigint(rand()) % (n - 1)) + 1;
+
+    bigint d = 1;
+
+
+    while (d == 1)
+    {
+        if (x == 0) {
+            x = (c + n) % n;
+        }
+        else {
+            x = (((x*x) % n) + c + n) % n;
+        }
+
+        if (y == 0) {
+            y = (c + n) % n;
+        }
+        else {
+            y = (((y*y) % n) + c + n) % n;
+        }
+
+
+        if (y == 0) {
+            y = (c + n) % n;
+        }
+        else {
+            y = (((y*y) % n) + c + n) % n;
+        }
+
+        if (x == 0 && y == 0) {
+            d = n;
+        }
+        else {
+            d = gcd((x - y).abs(), n);
+        }
+
+
+        if (d == n) return PollardRho(n);
     }
-    if( done ) break;
-  }
-  return k;
+    return d;
+}
+
+
+[[nodiscard]] std::vector<bigint> factorize(bigint n, std::vector<bigint> factors)
+{
+    if (n == 1)
+        return factors;
+    if (isPrime(n))
+    {
+
+            factors.push_back(n-1);
+        
+        
+        return factors;
+    }
+
+    bigint divisor = PollardRho(n);
+    factors = factorize(divisor, factors);
+    factors = factorize(n / divisor, factors);
+    return factors;
 }
 
 
 
 int main() {
 
-    bigint n = 99999999;
-    cout<<"Eylera : "<<phi(n)<<endl;
-    cout<<"Carmichael : "<<carmichael(9999)<<endl;
+    bigint n("234523452345234");
+
+    vector<bigint> vec;
+    vec=factorize(n,vec);
+    cout<<"Carmaicle: "<<carmaicle(vec,vec.size())<<endl;
+
+    cout<<"Eylera : "<<eurel(vec,vec.size())<<endl;
+
+    
     return 0;
 }
 
