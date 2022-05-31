00147928aa2fbb066df089811b3c5b6da7eb9893
Oleksandr <polishchuckalex@gmail.com>
2022-05-31 23:53:24 +0300

    Update PolyField.h

--- PolyField.h
+++ PolyField.h
@@ -30,8 +30,8 @@ public:
 
 	polynomial_ring add(const polynomial_ring& a, const polynomial_ring& b) const
 	{
-		checkDegree(a);
-		checkDegree(b);
+		//checkDegree(a);
+		//checkDegree(b);
 
 		polynomial_ring res = a + b;
 		//std::cout << res << std::endl;
@@ -41,8 +41,8 @@ public:
 
 	polynomial_ring subtract(const polynomial_ring& a, const polynomial_ring& b) const
 	{
-		checkDegree(a);
-		checkDegree(b);
+		//checkDegree(a);
+		//checkDegree(b);
 
 		polynomial_ring res = a - b;
 
@@ -52,8 +52,8 @@ public:
 
 	polynomial_ring multiply(const polynomial_ring& a, const polynomial_ring& b) const
 	{
-		checkDegree(a);
-		checkDegree(b);
+		//checkDegree(a);
+	//	checkDegree(b);
 
 		polynomial_ring res = a * b;
 
@@ -67,7 +67,7 @@ public:
 			throw std::invalid_argument("Power value must be greater than 0");
 		}
 
-		checkDegree(poly);
+		//checkDegree(poly);
 
 		polynomial_ring result({ 1_BI }, poly.getModulus());
 		polynomial_ring multiplier = poly;
@@ -85,12 +85,39 @@ public:
 
 		return result.remainder(result,irreducible);
 	}
+
+	//Bloshenko's code
+	void extended_Euclidean_algorithm(polynomial_ring a, polynomial_ring b, polynomial_ring& u, polynomial_ring& v, polynomial_ring& w, polynomial_ring& x, polynomial_ring& y, polynomial_ring& z) {
+
+		w = a;
+		z = b;
+
+		polynomial_ring q;
+
+		polynomial_ring one("1", a.getModulus());
+		while (!z.getData().empty()) {
+
+			q = w.divide(w, z);
+
+			u -= q * x;
+			v -= q * y;
+			w -= q * z;
+
+			std::swap(u, x);
+			std::swap(v, y);
+			std::swap(w, z);
+		}
+
+		u = u.remainder(u,irreducible);
+		v = v.remainder(v,irreducible);
+	}
+
 private:
 	polynomial_ring irreducible;
 
-	void checkDegree(const polynomial_ring& to_check) const {
+	/*void checkDegree(const polynomial_ring& to_check) const {
 		if (to_check.getDegree() > irreducible.getDegree()) {
 			throw std::invalid_argument("Polynomial isn't in the field");
 		}
-	}
+	}*/
 };
